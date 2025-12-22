e34379b29b3beae91add303565736e1c718095e9
Jorge Herraiz Soler <estudiantejorgeherraiz@gmail.com>
2025-12-22 18:34:20 +0100

    base de datos contratos, sin ia, simulitud palabras base de datos sqlite 3 datos vectorial

---
+++ Tema 3/vector_stores.py
@@ -0,0 +1,34 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+
+loader = PyPDFDirectoryLoader("Tema 3\\contratos")
+documentos = loader.load()
+
+print(f"Se cargaron {len(documentos)} documentos desde el directorio.")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+docs_split = text_splitter.split_documents(documentos)
+
+print(f"Se crearon {len(docs_split)} chunks de texto.")
+
+vectorstore = Chroma.from_documents(
+    docs_split,
+    embedding=OpenAIEmbeddings(model="text-embedding-3-large"),
+    persist_directory="Tema 3\\chroma_db"
+)
+consulta = "¿Dónde se encuentra el local del contrato en el que participa María Jiménez Campos?"
+
+resultados = vectorstore.similarity_search(consulta, k=2)
+
+print("Top 2 documentos mas similares a la consulta: \n")
+
+for i, doc in enumerate(resultados, start=1):
+    print(f"Contenido: {doc.page_content}")
+    print(f"Metadatos: {doc.metadata}")
